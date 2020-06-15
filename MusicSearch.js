562b16f87c920c036ab72dfa9b97f35f1fb3a10c
Diyar Kudrat <dkudrat@gmail.com>
2020-06-15 11:14:19 -0700

    created music search bar

---
+++ MusicSearch.js
@@ -0,0 +1,55 @@
+import { Component } from "react";
+import { SafeAreaView } from "react-native";
+
+
+
+const PAGE = 20;
+
+class MusicSearch extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            songs: [],
+            offset: 0,
+            query: '',
+            isFetching: false,
+            isEmpty: false,
+            spotify_token: null,
+            isTokenFetching: false,
+        };
+    }
+
+    handleSearchChange(text) {
+        this.setState(
+            {
+                isEmpty: false,
+                query: text,
+                offset: 0,
+                songs: []
+            },
+            () => {
+                
+            }
+        );
+    }
+
+    render() {
+        return (
+            <Container>
+                <SafeAreaView>
+                    <Header title="Music" />
+                    <SearchBar
+                        onChange={text => this.handleSearchChange(text)}
+                        text={query}
+                    />
+                    <SongQueue>
+
+                    </SongQueue>
+                </SafeAreaView>
+            </Container>
+        )
+    }
+}
+
+export default MusicSearch;
