a08faebd7e255f955c2d6db131562534c714cb01
Diyar Kudrat <dkudrat@gmail.com>
2020-06-15 12:15:10 -0700

    add API call to MusicSearch

---
+++ api/spotify_search.js
@@ -0,0 +1,45 @@
+const apiPrefix = 'https://api.spotify.com/v1';
+
+export default async ({
+    offset,
+    limit,
+    q,
+    spotify_token,
+}) => {
+        const uri = `${apiPrefix}/search?type=album,artist,track,playlist&limit=${limit}&offset=${offset}&q=${encodeUriComponent(q)}*`;
+        console.log('Begin Search, uri=', uri, 'token=', spotify_token);
+
+        const res = await fetch(uri, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer Token: ${spotify_token}`,
+            }
+        });
+
+        const json = await res.json;
+
+        if (!res.json) {
+            return [];
+        }
+
+        const {
+            tracks: {
+                items,
+            }
+        } = json;
+
+        return items.map(item => ({
+            id: item.id,
+            title: item.name,
+            popularity: item.popularity,
+            artist: item.artists
+                ? item.artists[0].name
+                : undefined,
+            albums: item.album.name,
+            is_playable: is_playable,
+            preview_url: preview_url,
+            imageUri: item.album.images
+                ? item.album.images[0].url
+                : undefined
+        }));
+};
